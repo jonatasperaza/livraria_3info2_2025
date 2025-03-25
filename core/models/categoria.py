e847008d326099ef71b59df4a8a86de6e8ef9d10
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-03-25 15:09:19 -0300

    Update Python version in configuration, add Categoria model and migration, and clean up dependencies

---
+++ core/models/categoria.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    """
+    Model representing a category.
+    """
+    descricao = models.CharField(max_length=100, unique=True, verbose_name='Descrição')
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.descricao
